5fcbd54923be1c3e7a0207bf2c5ba8ac79c4e8bc
unknown <emmanueljoeemie@gmail.com>
2024-03-06 15:03:09 -0800

    add pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
-  return <div className="text-red-400 flex">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/sign-up" element={<SignUp />} />
+
+        <Route path="/sign-in" element={<SignIn />} />
+
+        <Route path="/profile" element={<Profile />} />
+
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
-  
